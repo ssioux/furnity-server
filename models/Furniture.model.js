a8e86b591f30b897bfaf0f6068d5f2c5d82a32f2
David Lazaro <dlazaro7@gmail.com>
2022-12-23 07:40:30 +0100

    Furniture.model added

---
+++ models/Furniture.model.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+
+const furnitureSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "name is required"],
+    unique: true,
+    trim: true,
+  },
+
+  description: {
+    type: String,
+    required: [true, "description is required"],
+    trim: true,
+  },
+  image: {
+    type: String,
+    required: [true, "image is required"],
+  },
+  price: {
+    type: Number,
+    requered: [true, "price is required"],
+    trim: true,
+  },
+  onSale: Boolean,
+  salePrice: Number,
+  stock: Boolean,
+  units: Number,
+  category: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+  ],
+});
+
+const Furniture = model("Furniture", furnitureSchema);
+module.exports = Furniture;
