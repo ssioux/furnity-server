d07dc9b5683faaa81585cf3420a232fbfc8c2968
David Lazaro <dlazaro7@gmail.com>
2022-12-27 12:54:59 +0100

    User.routes added

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,4 +5,7 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
+const userRoutes = require("./user.routes");
+router.use("/user", userRoutes);
+
 module.exports = router;
