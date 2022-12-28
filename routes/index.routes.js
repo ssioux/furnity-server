0a550a2913a8d4014049785b1c3e963dcc4ba7c2
David Lazaro <dlazaro7@gmail.com>
2022-12-28 18:40:05 +0100

    indexRoutes.js Updated with addressRoutes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -8,10 +8,13 @@ router.get("/", (req, res, next) => {
 const userRoutes = require("./user.routes");
 router.use("/user", userRoutes);
 
-const furnitureRoutes = require("./furniture.routes")
-router.use("/furniture", furnitureRoutes)
+const furnitureRoutes = require("./furniture.routes");
+router.use("/furniture", furnitureRoutes);
 
 const orderRoutes = require("./order.routes");
 router.use("/order", orderRoutes);
+
+const addressRoutes = require("./address.routes");
+router.use("/address", addressRoutes);
 
 module.exports = router;
