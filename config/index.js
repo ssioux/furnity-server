522f88f296262cdc55e5198a7540e6b132157257
David Lazaro <dlazaro7@gmail.com>
2023-04-19 15:15:46 +0200

    cors updated

--- config/index.js
+++ config/index.js
@@ -27,7 +27,11 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  app.use(cors(corsOptions));
+  app.use(
+    cors({
+      origin: [FRONTEND_URL],
+    })
+  );
 
   // In development environment the app logs
   app.use(logger("dev"));
